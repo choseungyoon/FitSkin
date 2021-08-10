44989c3d1ff36155dc92757bd4bb5442bc8dcfd3
seungyoon <cho31250@gmail.com>
2021-08-11 01:15:20 +0900

    Set main grid layout

--- server/front/src/components/Main.vue
+++ server/front/src/components/Main.vue
@@ -1,22 +1,36 @@
 <template>
     <div class="main">
         <div class="container">
-            <header>Header</header>
-            <nav>Nav</nav>
-            <section>Section</section>
-            <aside>Aside-right</aside>
-            <footer>Footer</footer>
+          <Header />
+          <HelloWorld msg="Welcome to Your Vue.js App"/>
+          <Footer />
         </div>
     </div>
 </template>
 
 <script>
+import Header from './common/Header';
+import HelloWorld from './HelloWorld.vue'; 
+import Footer from './common/Footer';
+
 export default {
-    name: 'Main'
+    name: 'Main',
+    components : {
+        Header,
+        HelloWorld,
+        Footer
+    }
 }
 </script>
 
 <style scoped>
-@import '../../static/css/grid.css';
-@import '../../static/css/reset.css';
+html,body{padding:0; margin:0;}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin:0; padding:0;
+}
 </style>
