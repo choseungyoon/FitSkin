62921729e0c28145f3a8e9de219b40fde8e4fc3d
seungyoon <cho31250@gmail.com>
2021-08-10 20:32:34 +0900

    Add Main.vue

---
+++ server/front/src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="main">
+        <div class="container">
+            <header>Header</header>
+            <nav>Nav</nav>
+            <section>Section</section>
+            <aside>Aside-right</aside>
+            <footer>Footer</footer>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style scoped>
+@import '../../static/css/grid.css';
+@import '../../static/css/reset.css';
+</style>
