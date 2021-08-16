a501fcd9e89871163bf7a9efa40139067ff9abbb
choseungyoon <cho31250@gmail.com>
2021-08-16 13:56:46 +0900

    Set Layout and Card design in Product search page

--- server/front/src/components/common/Header.vue
+++ server/front/src/components/common/Header.vue
@@ -1,25 +1,43 @@
 <template>
 	<header>
-		<h1><a href="#" class="logo"><img alt="Vue logo" src="../../assets/fitskin_logo.png" width="200px"></a></h1>
+		<h1><router-link to="/" class="logo"><img alt="Vue logo" src="../../assets/fitskin_logo.png" width="200px"></router-link></h1>
 		<b-nav align = "center">
-			<b-nav-item active to="/">서비스 소개</b-nav-item>
-			<b-nav-item to="/product/search">제품검색</b-nav-item>
+			<b-nav-item to="/" exact>서비스소개</b-nav-item>
+			<b-nav-item to="/product/search">나만의 제품</b-nav-item>
 			<b-nav-item to="/FAQ">FAQ</b-nav-item>
 		</b-nav>
 	</header>
 </template>
 
 <script>
+
 export default {
     
 }
 </script>
 
 <style scoped>
-header{width:100%; text-align:center; position:relative; height:80px; border-bottom:1px solid #35495e}
-header h1{position:absolute; top:0; left:100px;}
-header ul.menu:after{display:block; clear:both; content:'';}
-header ul.menu{position:absolute; top:20px; right:50px;}
-header ul.menu li{float:left; padding:10px 20px; list-style:none;}
-a{text-decoration:none; color:#333;}
+header{
+	width:100%; 
+	text-align:center; 
+	position:relative; 
+	height:80px; 
+	border-bottom:1px solid #35495e}
+header h1{
+	position:absolute; 
+	top:0; 
+	left:100px;
+	}
+a{
+	text-decoration:none; 
+	color:#333;
+	padding-left: 35x;
+    padding-right: 35px;
+}
+a:hover , a.router-link-active{
+	font-weight: bold;
+	text-decoration: underline;
+	text-underline-position: under;
+	color:mediumaquamarine;
+}
 </style>
--- server/front/src/router/index.js
+++ server/front/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Main from '@/components/Main'
 import ProductSearch from '@/components/ProductSearch'
 import FAQ from '@/components/FAQ'
